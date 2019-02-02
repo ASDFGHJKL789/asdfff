3449d2b48f084822ac1d42076219178d9835c176
unknown <zinog@ace-mail.net>
2019-02-02 15:21:54 +0530

    i added new file

---
+++ tsd1.java
@@ -0,0 +1,40 @@
+class consdemo
+    {
+int x,y;
+consdemo()
+{
+    //x=m;
+    //y=n;
+System.out.println("cons 1")
+} 
+void fun()
+}
+       //System.out.println(x);
+       //System.out.println(y);
+       
+        System.out.println():
+}
+     
+.
+}
+
+
+void fun(float x)
+{
+
+          public static void main(String arg[])
+{
+      consdemo a = new consdemo(10,20);
+      consdemo a = new consdemo(20.5f,30);
+      consdemo a= new  consdemo(30,40);
+        //system.output.println(x
+ }
+   
+.a.fun();
+a.fun(2,2,5f);
+a.fun(2,5f);
+}
+{.
+
+
+
